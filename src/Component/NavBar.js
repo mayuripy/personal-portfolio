cb1fac7a4a3cbf9a6c6c6d3c1339c74f0a6d0389
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-01-06 14:41:34 +0530

    .....

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,6 +1,6 @@
 import {usestate,useEffect} from "react"
 import {NavBar, Container } from "react-bootstrap"
-import logo from "../assets/img/logo.svg"
+//import logo from "../assets/img/logo.svg"
 import navIcon1 from "../assets/img/nav-icon1.svg"
 import navIcon2 from "../assets/img/nav-icon2.svg"
 import navIcon3 from "../assets/img/nav-icon3.svg"
@@ -19,16 +19,20 @@ export const NavBar = () => {
         setscrolled(false)
      }
      }
+
     window.addEventListener("scroll",onScroll);
-    return () => window.removeEventListener("scroll",onScroll)
+    return () => window.removeEventListener("scroll",onScroll);
 
     
-     }, []);
+     }, [])
      const onupdatActiveLink = () => {
         setActiveLink(value);
      }
+     const onUpdateActiveLink =(value) => {
+        serActivLink(value);
+     }
    
-    return
+    return(
         <NavBar  expand="lg">
         <Container >
         <NavBar.Brand href="#home">
@@ -46,10 +50,10 @@ export const NavBar = () => {
         </Nav>
         <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src{} alt=""/></a>
-                <a href="#"><img src{} alt=""/></a>
-                <a href="#"><img src{} alt=""/></a>
-                <a href="#"><img src{} alt=""/></a>
+                <a href="#"><img src={navIcon1} alt=""/></a>
+                <a href="#"><img src={navIcon2} alt=""/></a>
+                <a href="#"><img src={navIcon3} alt=""/></a>
+                
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
         </span>
@@ -57,4 +61,4 @@ export const NavBar = () => {
         </Container>
         </NavBar>
     )
-}
+    
