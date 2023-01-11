78966d0662063f4b988f03b239461ed1ee83c563
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-01-11 23:55:07 +0530

    gif

--- src/Component/Banner.js
+++ src/Component/Banner.js
@@ -7,10 +7,13 @@ export const Banner = () => {
         <Row className="align-item-center">
             <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my portfolio</span>
-            <h1>('Hi i'm sebdecoded')<span className="wrap">Web devlope</span></h1>
+            <h1>('Hi i'm sebdecoded')<span className="wrap">Web devloper</span></h1>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
              Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
              <button onClick={() => console.log('connect')}>let's Connect <ArrowRightCircle size={25} /></button>
+            </Col>
+            <Col  xs={12} md={6} xl={5}>
+              <img src={header-img} alt="header img" />
             </Col>
         </Row>
     </Container>
